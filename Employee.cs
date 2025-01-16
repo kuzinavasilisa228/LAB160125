f8ade9a391450840d9e7bb336f332d23d47963f8
CodeDevelopment <codedevelopment@inbox.eu>
2025-01-16 12:06:24 +0300

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB160125
+{
+    class Employee
+    {
+        public string FullName { get; set; }
+        public double Salary { get; set; }
+        public void Show()
+        {
+            Console.WriteLine("ФИО: " + FullName);
+            Console.WriteLine("Зарплата: " + Salary);
+        }
+        public double CalculateAnnualSalary()
+        {
+            return Salary * 12;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB160125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FullName = "Иванов";
+            employee.Salary = 63500;
+            PrintEmployeeInfo(employee);
+            double annualSalary = employee.CalculateAnnualSalary();
+            Console.WriteLine("Годовая зарплата: " + annualSalary);
+            Console.ReadKey();
+        }
+        static void PrintEmployeeInfo(Employee emp)
+        {
+            Console.WriteLine("Информация о сотруднике:");
+            emp.Show();
+        }
+    }
+}
